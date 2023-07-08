bd2dbb82b239f0736a0d972b5b95ceb260715ae0
Dong205 <136000020+Dong205@users.noreply.github.com>
2023-07-08 10:51:55 +0700

    update 1

--- thực hành 4/ex01.js
+++ thực hành 4/ex01.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
     hh = h / 2,
     
     opts = {
-      strings: [ 'HAPPY', 'BIRTHDAY!', 'ME LU' ],
+      strings: [ 'HAPPY', 'BIRTHDAY!', 'DONG LU' ],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
